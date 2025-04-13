76be7f8728ef2d88dead4254d36f277f7ec5aaea
Shchok <shchok2017@gmail.com>
2025-04-13 22:03:15 +0300

    homework1

---
+++ Homework1.js
@@ -0,0 +1,36 @@
+//const isAdult = confirm("Чи досягли ви повнолітнього віку?");
+//console.log(isAdult);
+
+//let firstName = "Oksana";
+//let lastName = "Shcherba";
+//let studyGroup = "AFH";
+//let birthYear = 1980;
+//let isMarried = true;
+//console.log("Number:", birthYear);
+//console.log("Boolean:", isMarried);
+//console.log("String:", firstName, lastName, studyGroup);
+
+//let nullVariable = null;
+//let undefinedVariable = undefined;
+
+//console.log("null:", nullVariable);
+//console.log("undefined:", undefinedVariable);
+
+// let login = prompt("Введіть ваш логін:");
+// let email = prompt("Введіть ваш емейл:");
+// let password = prompt("Введіть ваш пароль:");
+
+// alert(`Dear ${login}, your email is ${email}, your password is ${password}`);
+
+// const secondsInMinute = 60;
+// const minutesInHour = 60;
+// const hoursInDay = 24;
+// const daysInMonth = 30.44;
+
+// const secondsInHour = secondsInMinute * minutesInHour;
+// const secondsInDay = secondsInHour * hoursInDay;
+// const secondsInMonth = secondsInDay * daysInMonth;
+
+// alert("Кількість секунд в годині:" + secondsInHour);
+// alert("Кількість секунд в добі:" + secondsInDay);
+// alert("Кількість секунд в місяці:" + secondsInMonth);
